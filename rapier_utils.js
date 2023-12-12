c8c255391594bf3b9c650aa5698b9f03d4f1d04d
jumpjack <jumpjack@libero.it>
2023-12-12 11:23:33 +0100

    Update rapier_utils.js

--- rapier_utils.js
+++ rapier_utils.js
@@ -1,5 +1,5 @@
 
-import * as THREE from 'three';
+import * as THREE from './three.module-149.js';
 import RAPIER from './rapier3d-compat.js';
 
 export function getColliderDesc(world, scene, f, width, height, depth, color=0x333333) {
