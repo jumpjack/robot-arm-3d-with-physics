93321ba96c5a654fb6e526b332ddee81fa3b2ec0
jumpjack <jumpjack@libero.it>
2023-12-13 19:22:28 +0100

    Update robot-debug.js

--- robot-debug.js
+++ robot-debug.js
@@ -121,15 +121,15 @@ function revoluteJoint(world, r1, r2, axis, x1=0, y1=0, z1=0, x2=0, y2=0, z2=0) 
 
         this.j0 = utils.fixedJoint(world, this.base, this.mast, 0, this.base.h/2, 0, 0, -this.mast.h/2, 0);
         this.ji = utils.fixedJoint(world, this.base, this.indicator, 0, this.base.h/2, this.base.w/2);
-        this.j1 = utils.revoluteJoint(world, this.mast, this.arm_base, y, 0, 0, 0, -arm_w*0.75, 0, 0);
+          this.jother = utils.revoluteJoint(world, this.mast, this.shoulder2, x,  -arm_w/2, 0, this.shoulder.d/2-arm_w/2,  arm_w/2, 0, -this.elbow.d/2);
+        this.j1 = utils.revoluteJoint(world, this.shoulder2, this.arm_base, y, 0, 0, 0, -arm_w*0.75, 0, 0);
         this.j2 = utils.revoluteJoint(world, this.arm_base, this.shoulder, x, this.arm_base.w/2, 0, 0, -arm_w/2, 0, -this.shoulder.d/2);
-          this.j2b = utils.revoluteJoint(world, this.shoulder, this.shoulder2, x,  -arm_w/2, 0, this.shoulder.d/2-arm_w/2,  arm_w/2, 0, -this.elbow.d/2);
         this.j3 = utils.revoluteJoint(world, this.shoulder, this.elbow, y,  -arm_w/2, 0, this.shoulder.d/2-arm_w/2,  arm_w/2, 0, -this.elbow.d/2);
         this.j4 = utils.revoluteJoint(world, this.elbow, this.forearm, z, 0, 0, this.elbow.d/2, 0, 0, -this.forearm.d/2);
         this.j5 = utils.revoluteJoint(world, this.forearm, this.wrist, x, arm_w/2, 0, this.forearm.d/2-arm_w/2, -arm_w/2, 0, -this.wrist.d/2);
         this.j6 = utils.revoluteJoint(world, this.wrist, this.g3, z, 0, 0, this.wrist.d/2, 0, 0, -this.g3.d/2);
 
-        this.joints.push(this.j0, this.j1, this.j2, this.j3, this.j4, this.j5, this.j6);
+        this.joints.push(this.j0, this.j1, this.j2, this.j3, this.j4, this.j5, this.j6, this.jother);
         this.j1.setContactsEnabled(false);
         this.ji.setContactsEnabled(false);
     }
